4ab2184441dbf6dc5bf0100fe21f968cd1553d2f
jsyoon <jsyoon@shinwooend.co.kr>
2017-05-30 17:29:54 +0900

    OnDateChangeListener 추가

--- app/src/main/java/com/jsyoon/sleepmask2/TabFragment2.java
+++ app/src/main/java/com/jsyoon/sleepmask2/TabFragment2.java
@@ -7,10 +7,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 
 public class TabFragment2 extends Fragment {
     private static final String TAG = "TabFragment2";
+    CalendarView calendarView;
+    View frag2view;
 
     public TabFragment2() {
         // Required empty public constructor
@@ -19,18 +23,28 @@ public class TabFragment2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
         }
         Log.d(TAG, "onCreate");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.tab_fragment2, container, false);
+        frag2view = inflater.inflate(R.layout.tab_fragment2, container, false);
+
+        calendarView = (CalendarView) frag2view.findViewById(R.id.calendarView2);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                Toast.makeText(frag2view.getContext(), "Selected Date:\n" + "Day = " + i2 + "\n" + "Month = " + i1 + "\n" + "Year = " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Log.d(TAG, "onCreateView");
-        return view;
+        return frag2view;
     }
 
     @Override
