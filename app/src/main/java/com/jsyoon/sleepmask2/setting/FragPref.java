3d880eb8d10629d2eb732b0eb7d6e18972b91763
jsyoon <jsyoon@shinwooend.co.kr>
2017-05-30 10:15:01 +0900

    Setting Screen에 Version 정보 표시 기능

--- app/src/main/java/com/jsyoon/sleepmask2/setting/FragPref.java
+++ app/src/main/java/com/jsyoon/sleepmask2/setting/FragPref.java
@@ -1,6 +1,7 @@
 package com.jsyoon.sleepmask2.setting;
 
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.util.Log;
 
@@ -13,6 +14,17 @@ public class FragPref extends PreferenceFragmentCompat {
 
         // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
         addPreferencesFromResource(R.xml.setting_pref);
+
+        Preference button = findPreference("VersionInfo");
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                //code for what you want it to do
+                preference.setSummary("Button Clicked");
+                return true;
+            }
+        });
+
         Log.d(TAG, "onCreatePreferences");
     }
 }
